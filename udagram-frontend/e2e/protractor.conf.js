f8c40d3e13704026826ae56d3318d74a66b99ade
Aml <amal.elsayed43@gmail.com>
2022-02-04 05:30:08 +0200

    deploy frontend

--- udagram-frontend/e2e/protractor.conf.js
+++ udagram-frontend/e2e/protractor.conf.js
@@ -9,10 +9,7 @@ exports.config = {
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome',
-    'chromeOptions': {
-      'args': ['no-sandbox']
-    }
+    'browserName': 'chrome'
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
